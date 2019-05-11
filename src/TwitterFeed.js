3af5356fe94aa72c98a2eefdb1b7b45df4cfde1b
Elli <ellimoty@gmail.com>
2019-05-11 15:27:27 +1000

    add subscription

--- src/TwitterFeed.js
+++ src/TwitterFeed.js
@@ -2,15 +2,32 @@ import React, { Component } from "react";
 import { graphql, compose } from "react-apollo";
 import { Container, Row, Col } from "react-bootstrap";
 import { TWITTER_FEED_QUERY } from "./data/queries";
+import { NEW_TWEET_SUBSCRIPTION } from "./data/subscriptions";
 import TweetList from "./components/TweetList";
 import TweetForm from "./components/TweetForm";
 
 class TwitterFeed extends Component {
-
-  handleSubmitSuccess = async () => {
-    this.props.tweets.refetch();
-  }
   
+  componentDidMount() {
+    this.props.tweets.subscribeToMore({
+      document: NEW_TWEET_SUBSCRIPTION,
+      updateQuery: (prevData, { subscriptionData }) => {
+        if (!subscriptionData) {
+          return prevData;
+        }
+        const newTweetItem = subscriptionData.data.Tweet.node;
+        return {
+          ...prevData,
+          allTweets: [newTweetItem, ...prevData.allTweets]
+        };
+      }
+    });
+  }
+
+  handleSubmitSuccess = () => {
+    this.props.tweets.refetch();
+  };
+
   render() {
     const { author, tweets } = this.props;
     // console.log('tweets: ', tweets);
@@ -18,7 +35,10 @@ class TwitterFeed extends Component {
       <Container>
         <Row>
           <Col sm={5}>
-            <TweetForm handleSubmitSuccess={this.handleSubmitSuccess} author={author}/>
+            <TweetForm
+              handleSubmitSuccess={this.handleSubmitSuccess}
+              author={author}
+            />
           </Col>
           <Col sm={7}>
             <TweetList tweets={tweets.allTweets} loading={tweets.loading} />
