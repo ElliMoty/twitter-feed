e10f7642e1a6eb759ec82f78650c382fe4c08dff
Elli <ellimoty@gmail.com>
2019-05-11 12:25:47 +1000

    add tweetList and queries file

---
+++ src/components/TweetList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TweetList = props => {
+  //   console.log("is loading: ", props.loading);
+  if (props.loading) {
+    return <div>We are loading...</div>;
+  }
+  return props.tweets.map(tweet => (
+    <div className="tweet-list__tweet--container" key={tweet.id}>
+      <h5 className="tweet-list__tweet--author">{tweet.author}</h5>
+      <span className="tweet-list__tweet--message">{tweet.message}</span>
+    </div>
+  ));
+};
+
+export default TweetList;
